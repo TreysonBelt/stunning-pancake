0724fcd3dc03bd5269a9f56eda742ef3f56fc616
TreysonBelt <treysonbelt@gmail.com>
2024-02-12 20:55:30 -0700

    RobotCommand

--- Program.cs
+++ Program.cs
@@ -8,171 +8,104 @@ namespace StunningPancake
         public static void Main()
         {
             Console.Clear();
-            double num = 10;
-            double weight = 25;
-            double volume = 20.1;
-            Pack pack = new Pack(num, weight, volume);
-            bool end = true;
-            while (end)
+            Robot robot = new Robot();
+            for (int i = 0; i < robot.Commands.Length; i++)
             {
-                System.Console.Write("What would you like to do? ");
-                string input = Console.ReadLine();
-                if (input == "add sword")
+                string input1 = Console.ReadLine();
+                if (input1 == "on")
                 {
-                    Sword sword = new Sword();
-                    pack.Add(sword);
+                    OnCommand onCommand = new OnCommand();
+                    robot.Commands[i] = onCommand;
                 }
-                else if (input == "add arrow")
+                else if (input1 == "off")
                 {
-                    Arrow arrow = new Arrow();
-                    pack.Add(arrow);
+                    OffCommand offCommand = new OffCommand();
+                    robot.Commands[i] = offCommand;
                 }
-                else if (input == "add bow")
+                else if (input1 == "north")
                 {
-                    Bow bow = new Bow();
-                    pack.Add(bow);
+                    NorthCommand northCommand = new NorthCommand();
+                    robot.Commands[i] = northCommand;
                 }
-                else if (input == "add rope")
+                else if (input1 == "south")
                 {
-                    Rope rope = new Rope();
-                    pack.Add(rope);
+                    SouthCommand southCommand = new SouthCommand();
+                    robot.Commands[i] = southCommand;
                 }
-                else if (input == "add water")
+                else if (input1 == "west")
                 {
-                    Water water = new Water();
-                    pack.Add(water);
+                    WestCommand westCommand = new WestCommand();
+                    robot.Commands[i] = westCommand;
                 }
-                else if (input == "add food")
+                else if (input1 == "east")
                 {
-                    Food food = new Food();
-                    pack.Add(food);
+                    EastCommand eastCommand = new EastCommand();
+                    robot.Commands[i] = eastCommand;
                 }
-                else if (input == "end")
-                {
-                    end = false;
-                }
-                else if (input =="check pack")
-                {
-                    pack.Display();
-                }
-                else if (input == "menu")
-                {
-                    System.Console.WriteLine("You can add a sword, arrow, bow, rope, water, food, or check your pack. input end if you wish to end simulation.");
-                }
-                else
-                {
-                    System.Console.WriteLine("please insert an accepted command.");
-                }
+            }
+                robot.Run();
+        }
+    }
+    public class Robot
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsPowered { get; set; }
+        public IRobotCommand?[] Commands { get; } = new IRobotCommand?[3];
+        public void Run()
+        {
+            foreach (IRobotCommand? command in Commands)
+            {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
             }
         }
     }
-    class InventoryItem
+    public interface IRobotCommand
     {
-        public double weight {get; protected set;}
-        public double volume {get; protected set;}
-        public InventoryItem()
+        void Run(Robot robot);
+    }
+    public class OnCommand : IRobotCommand
+    {
+        public void Run(Robot robot)
         {
-            weight = 0.01;
-            volume = 0.01;
-        }
-        public InventoryItem(double Weight, double Volume)
-        {
-            weight = Weight;
-            volume = Volume;
+            robot.IsPowered = true;
         }
     }
-    class Arrow : InventoryItem
+    public class OffCommand : IRobotCommand
     {
-        public Arrow()
+        public void Run(Robot robot)
         {
-            weight = 0.01;
-            volume = 0.05;
+            robot.IsPowered = false;
         }
     }
-    class Bow : InventoryItem
+    public class NorthCommand : IRobotCommand
     {
-        public Bow()
+        public void Run(Robot robot)
         {
-            weight = 1;
-            volume = 4;
+            robot.Y = robot.Y + 1;
         }
     }
-    class Rope : InventoryItem
+    public class SouthCommand : IRobotCommand
     {
-        public Rope()
+        public void Run(Robot robot)
         {
-            weight = 1;
-            volume = 1.5;
+            robot.Y = robot.Y - 1;
         }
     }
-    class Water : InventoryItem
+    public class EastCommand : IRobotCommand
     {
-        public Water()
+        public void Run(Robot robot)
         {
-            weight = 2;
-            volume = 3;
+            robot.X = robot.X + 1;
         }
     }
-    class Food : InventoryItem
+    public class WestCommand : IRobotCommand
     {
-        public Food()
+        public void Run(Robot robot)
         {
-            weight = 1;
-            volume = 0.5;
+            robot.X = robot.X - 1;
         }
     }
-    class Sword : InventoryItem
-    {
-        public Sword()
-        {
-            weight = 5;
-            volume = 3;
-        }
-    }
-    class Pack
-    {
-        public double numMax {get;}
-        public double weightMax {get;}
-        public double volumeMax {get;}
-        private double num;
-        private double Weight;
-        private double Volume;
-        bool isPackFull = false;
-        List<InventoryItem> packList = new List<InventoryItem>();
-        public Pack(double Num, double Weight, double Volume)
-        {
-            numMax = Num;
-            weightMax = Weight;
-            volumeMax = Volume;
-        }
-        public bool Add(InventoryItem item)
-        {
-            num ++;
-            Weight = Weight + item.weight;
-            Volume = Volume + item.volume;
-            if ((num >= numMax) || (Weight >= weightMax) || (Volume == volumeMax))
-            {
-                num = num - 1;
-                Weight = Weight - item.weight;
-                Volume = Volume - item.volume;
-                System.Console.WriteLine("Exceeds pack requirements.");
-                isPackFull = false;
-            }
-            else
-            {
-                packList.Add(item);
-                System.Console.WriteLine("Item inserted.");
-                isPackFull = true;
-            }
-            return isPackFull;
-        }
-        public void Display()
-        {
-            foreach (InventoryItem item in packList)
-            {
-                System.Console.WriteLine($"Item: {item} weight: {item.weight} volume: {item.volume}");
-            }
-            System.Console.WriteLine($"current weight and volume free: {(weightMax - Weight)}, {(volumeMax - Volume)}");
-        }
-    }
+
 }
